a94652d2ddbaf9a94e0915ee7720f67ef72541ac
Aashiqara <rashique65@gmail.com>
2021-12-02 22:29:40 +0545

    message

---
+++ lab6.py
@@ -0,0 +1,4 @@
+#wap to add even numbers of first 10 positive integers
+listnum=[2,4,6,8,10,12,14,16,18,20]
+sumeven=sum(listnum)
+print("sum of ten even numbers",sumeven)
---
+++ lab7.py
@@ -0,0 +1,4 @@
+#wap to add odd number of first positive 10 integers
+mylist=[1,3,5,7,9,11,13,15,17,19]
+sumodd=sum(mylist)
+print("sum of first ten odd numbers is",sumodd)
